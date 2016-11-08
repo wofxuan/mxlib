a5e392e3521400de5316ed8f43ce2cb663a17ae5
mx <171377682@qq.com>
2016-11-08 10:59:02 +0800

    测试

--- src/main/java/com/android/mx/mxlib/FileSelectActivity.java
+++ src/main/java/com/android/mx/mxlib/FileSelectActivity.java
@@ -1,7 +1,7 @@
 package com.android.mx.mxlib;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class FileSelectActivity extends AppCompatActivity {
 
@@ -9,5 +9,7 @@ public class FileSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_select);
+
+
     }
 }
